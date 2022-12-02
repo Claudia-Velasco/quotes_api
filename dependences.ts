20f53544316c3616ea3a7aa85d1afcfeca94eb63
Claudia-Velasco <29lupitha@gmail.com>
2022-12-01 22:47:04 -0600

    first commit

---
+++ dependences.ts
@@ -0,0 +1,48 @@
+import { Application, Context, Router } from "https://deno.land/x/oak/mod.ts";
+
+import { Status, STATUS_TEXT } from "https://deno.land/std/http/http_status.ts";
+
+import { expect } from "https://deno.land/x/expect/mod.ts";
+
+import {
+  create as jwtCreate,
+  decode as jwtDecode,
+  getNumericDate,
+  verify as jwtVerify,
+} from "https://deno.land/x/djwt/mod.ts";
+
+import type {
+  Header as jwtHeader,
+  Payload as jwtPayload,
+} from "https://deno.land/x/djwt/mod.ts";
+
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+
+import sha512 from "./utils/sha512.ts";
+
+import { Bson, MongoClient } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
+
+import { Client as ClientMariaDB } from "https://deno.land/x/mysql/mod.ts";
+
+export const { JWT_SECRET } = config({ safe: true });
+
+export {
+  Application,
+  config,
+  Context,
+  expect,
+  getNumericDate,
+  jwtCreate,
+  jwtDecode,
+  jwtVerify,
+  Router,
+  Status,
+  STATUS_TEXT,
+};
+
+export type { jwtHeader, jwtPayload };
+
+export default sha512;
+
+export { Bson, MongoClient };
+export { ClientMariaDB };
